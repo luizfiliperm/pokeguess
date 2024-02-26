c69fbbd063a2231e80248bab368e891330c8637c
luizfiliperm <luizfilipemr17@gmail.com>
2024-02-26 15:38:37 -0300

    Creating Pokémon entity

---
+++ src/main/java/com/luizfilipemr/pokeguess/entities/Pokemon.java
@@ -0,0 +1,17 @@
+package com.luizfilipemr.pokeguess.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Pokemon {
+
+    private Integer number;
+
+    private String name;
+
+    private String sprite;
+}
