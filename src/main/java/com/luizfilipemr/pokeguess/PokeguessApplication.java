534b80aba21170243a60cf5026f9b9a48bf53890
luizfiliperm <luizfilipemr17@gmail.com>
2024-02-26 15:28:09 -0300

    First Commit

---
+++ src/main/java/com/luizfilipemr/pokeguess/PokeguessApplication.java
@@ -0,0 +1,13 @@
+package com.luizfilipemr.pokeguess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeguessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeguessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizfilipemr/pokeguess/PokeguessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizfilipemr.pokeguess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeguessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
