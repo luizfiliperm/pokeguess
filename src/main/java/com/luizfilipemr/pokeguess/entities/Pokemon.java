8428571bf02599536c9b0ca2886d72b9dd70dafb
Luiz Filipe <79930607+luizfiliperm@users.noreply.github.com>
2025-08-21 20:15:24 -0300

    fix: remove lombok

--- src/main/java/com/luizfilipemr/pokeguess/entities/Pokemon.java
+++ src/main/java/com/luizfilipemr/pokeguess/entities/Pokemon.java
@@ -1,19 +1,50 @@
 package com.luizfilipemr.pokeguess.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Data
 public class Pokemon {
 
     private Integer position;
-
     private Integer number;
-
     private String name;
-
     private String sprite;
+
+    public Pokemon(Integer position, Integer number, String name, String sprite) {
+        this.position = position;
+        this.number = number;
+        this.name = name;
+        this.sprite = sprite;
+    }
+
+    public Pokemon() {}
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public void setNumber(Integer number) {
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(String sprite) {
+        this.sprite = sprite;
+    }
 }
